84ff76bfcc10b41eab6f184b2135ad96c8bdd0dc
Arisha Asif <arisha.github@gmail.com>
2025-03-25 18:17:17 +0500

    changes Shop.css to shop.css

--- bookstore/src/components/Shop.jsx
+++ bookstore/src/components/Shop.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/Shop.css";
+import "../styles/shop.css";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const Shop = () => {
